c543cee157a0c52a8ad2b3ba2660f8866d0ac751
thous210 <thousdev@gmail.com>
2022-12-07 13:29:41 +0900

    [upd] submitter

--- submitter.js
+++ submitter.js
@@ -1,12 +1,26 @@
 function submitForm() {
-    var data = {};
-    data["amount"] = document.myForm.amount.value;
-    data["price"] = document.myForm.price.value;
-    data["event_name"] = document.myForm.event_name.value;
-    data["image"] = document.myForm.image.value;
-    data["explanation"] = document.myForm.description.value;
-    data["date"] = document.myForm.date.value;
+    var form = document.getElementById("myForm");
 
-    json_data = JSON.stringify(data);
-    document.myForm.submit();
+    var data = {};
+    data["amount"] = form.elements.amount.value;
+    data["price"] = form.elements.price.value;
+    data["event_name"] = form.elements.event_name.value;
+    data["image"] = form.elements.image.value;
+    data["explanation"] = form.elements.description.value;
+    data["date"] = form.elements.date.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8080/new', true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify(data));
 }
+
+/*
+fetch('http://localhost:8080/new', {
+  method: 'POST',
+  body: jsonData
+})
+.then(function(response) {
+  // サーバからのレスポンスを処理する
+});
+*/
