daa8c830c620db9139c83cea0390f6c0c1b1426b
kana-lab <kirinokanatrust@gmail.com>
2022-12-07 15:28:24 +0900

    removed date & small fixes

--- submitter.js
+++ submitter.js
@@ -2,25 +2,14 @@ function submitForm() {
     var form = document.getElementById("myForm");
 
     var data = {};
-    data["amount"] = form.elements.amount.value;
-    data["price"] = form.elements.price.value;
+    data["amount"] = parseInt(form.elements.amount.value);
+    data["price"] = parseFloat(form.elements.price.value);
     data["event_name"] = form.elements.event_name.value;
     data["image"] = form.elements.image.value;
     data["explanation"] = form.elements.description.value;
-    data["date"] = form.elements.date.value;
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8080/new', true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(data));
 }
-
-/*
-fetch('http://localhost:8080/new', {
-  method: 'POST',
-  body: jsonData
-})
-.then(function(response) {
-  // サーバからのレスポンスを処理する
-});
-*/
